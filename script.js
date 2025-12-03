9729c237f051fc660af26fd3ae80766d073e4b5e
aswanthkt31 <aswanthkt99@gmail.com>
2025-12-03 14:51:10 +0530

    Add featured products

--- script.js
+++ script.js
@@ -10,4 +10,19 @@ leftBtn.addEventListener("click", (e) => {
 rightBtn.addEventListener("click", (e) => {
     e.preventDefault();
     scrollCate.scrollLeft += scrollAmount;
+});
+
+// Product-card-scrolling
+const productLeftBtn = document.getElementById("product-left-btn");
+const productRightBtn = document.getElementById("product-right-btn");
+const scrollProduct = document.getElementById("scroll-product");
+
+let productScrollAmount = 280;
+productLeftBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    scrollProduct.scrollLeft -= productScrollAmount
+})
+productRightBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    scrollProduct.scrollLeft += productScrollAmount;
 })
